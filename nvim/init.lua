980feaca85c2683bd0da2ccde12754f708e4ba29
rqdmap <rqdmap@foxmail.com>
2023-03-05 16:00:01 +0800

    定时备份

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,3 +73,52 @@ vim.api.nvim_create_autocmd("BufReadPost",{
 vim.o["path"] = "."
 vim.o["path"] = vim.fn.getcwd() .. "/**," .. vim.o["path"];
 
+
+-- Quite ugly... Though works well... 后期一定会改进的...
+-- https://vi.stackexchange.com/questions/41405/how-to-extract-filename-of-w-command-in-vims-autocmd
+vim.cmd([[
+	function! CorrectWritePath()
+	  if getcmdtype() != ':'
+		return "\<CR>"
+	  endif
+
+	  if matchstr(getcmdline(), '^w\%[rite]!\?\>') == ''
+		return "\<CR>"
+	  endif
+
+	  let l:cmdline = getcmdline()
+
+	  " Parse the cmd line
+	  let l:newcmdline = l:cmdline
+	  let l:good = v:true
+	  if matchstr(l:cmdline, ']') != ''
+		echo 'match'
+		let l:newcmdline = substitute(l:cmdline, ']', '\', 'g')
+		let l:good = v:false
+	  endif
+	  if matchstr(l:cmdline, '[') != ''
+		echo 'match'
+		let l:newcmdline = substitute(l:cmdline, '[', '\', 'g')
+		let l:good = v:false
+	  endif
+	  if matchstr(l:cmdline, '`') != ''
+		echo 'match'
+		let l:newcmdline = substitute(l:cmdline, '`', '\', 'g')
+		let l:good = v:false
+	  endif
+	  if matchstr(l:cmdline, '=') != ''
+		echo 'match'
+		let l:newcmdline = substitute(l:cmdline, '=', '\', 'g')
+		let l:good = v:false
+	  endif
+
+	  if l:good
+		return "\<CR>"
+	  else
+		return "\<C-u>" . l:newcmdline  
+	  endif
+	endfunction
+]])
+vim.cmd([[cnoremap <expr> <CR> CorrectWritePath()]])
+
+
