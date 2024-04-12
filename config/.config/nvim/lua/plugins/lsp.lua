6966c8d8bc12702d538e10260b2024cf3769846f
rqdmap <55649208+rqdmap@users.noreply.github.com>
2024-04-12 16:23:20 +0800

    添加 clangd 配置

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -100,6 +100,8 @@ return {
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+			lspconfig.clangd.setup {}
+
 			lspconfig.cssls.setup {
 				capabilities = capabilities,
 			}
