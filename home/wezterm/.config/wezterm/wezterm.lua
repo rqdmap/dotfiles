0504be4fb23963304e60f5094a2fa3a099763ead
rqdmap <55649208+rqdmap@users.noreply.github.com>
2025-11-26 22:08:04 +0800

    更新 Wezterm 终端配置, 优化性能表现

--- home/wezterm/.config/wezterm/wezterm.lua
+++ home/wezterm/.config/wezterm/wezterm.lua
@@ -1,18 +1,21 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
--- 字体配置
+-- 字体配置 
 config.font = wezterm.font_with_fallback({
     {
         family = 'Iosevka Nerd Font',
         harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
     },
-    'Source Han Sans CN',
+    'Source Han Sans SC',
     'Noto Color Emoji',
 })
-config.font_size = 13
+config.font_size = 16
 config.unicode_version = 14
 config.warn_about_missing_glyphs = false
+
+-- 隐藏标题栏
+config.window_decorations = "RESIZE"
 
 -- 自动使用暗色色调
 config.bold_brightens_ansi_colors = false
@@ -28,6 +31,8 @@ config.window_padding = {
 config.enable_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
 
+-- 性能配置
+config.max_fps = 120
 
 -- 键盘快捷键
 config.keys = {
@@ -47,6 +52,18 @@ config.keys = {
     mods = 'CTRL',
     action = wezterm.action.SendString(string.char(0x1f)),
   },
+  {
+    key = '.',
+    mods = 'CTRL',
+    action = wezterm.action.SendKey { key = '.', mods = 'CTRL' },
+  },
+
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentTab { confirm = false },
+  },
+
 }
 
 return config
